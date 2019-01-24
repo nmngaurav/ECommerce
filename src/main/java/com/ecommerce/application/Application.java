f72b105d0ef15ce0d7d970ca83640b9e8ce2cabb
GAURAV SINGH <singhnmngaurav@gmail.com>
2019-01-25 00:04:45 +0530

    added basic dependencies

--- src/main/java/com/ecommerce/application/Application.java
+++ src/main/java/com/ecommerce/application/Application.java
@@ -2,13 +2,12 @@ package com.ecommerce.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-@EnableMongoRepositories({ "com.ecommerce.application.repository" })
+//@EnableMongoRepositories({ "com.ecommerce.application.repository" })
 public class Application {
 
 	public static void main(String[] args) throws Exception {
